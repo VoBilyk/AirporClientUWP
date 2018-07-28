c430bf19c5e9718df1c7fbd58063c545d8044a94
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-28 18:58:38 +0300

    implemented pilot view and main menu

---
+++ AirporClientUWP/Models/Pilot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace AirporClientUWP.Models
+{
+    public class Pilot
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Experience { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public List<Guid> CrewsId { get; set; }
+    }
+}
---
+++ AirporClientUWP/Views/PilotsView.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+using AirporClientUWP.Services;
+using AirporClientUWP.Models;
+
+namespace AirporClientUWP.Views
+{
+    public sealed partial class PilotsView : Page
+    {
+        private PilotService _service;
+
+        public PilotsView()
+        {
+            this.InitializeComponent();
+            _service = new PilotService();
+            pilots = _service.GetAllAsync().Result;
+        }
+
+        public ObservableCollection<Pilot> pilots;
+
+
+        public async Task<ObservableCollection<Pilot>> GetPilots()
+        {
+            var service = new PilotService();
+            pilots = await service.GetAllAsync();
+            return pilots;
+        }
+    }
+}
