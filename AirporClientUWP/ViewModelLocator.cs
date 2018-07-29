9f80bf0bb83707b1e2a8b738e377e0095ecfb5d1
Volodymyr <34250821+VoBilyk@users.noreply.github.com>
2018-07-29 20:58:40 +0300

    registered vm in locator

--- AirporClientUWP/ViewModelLocator.cs
+++ AirporClientUWP/ViewModelLocator.cs
@@ -18,16 +18,31 @@ namespace AirporClientUWP
             //Register your services used here
             SimpleIoc.Default.Register<INavigationService>(() => navigationService);
             SimpleIoc.Default.Register<PilotViewModel>();
+            SimpleIoc.Default.Register<StewardessViewModel>();
+            SimpleIoc.Default.Register<CrewViewModel>();
+            SimpleIoc.Default.Register<DepartureViewModel>();
+            SimpleIoc.Default.Register<FlightViewModel>();
+            SimpleIoc.Default.Register<AeroplaneTypeViewModel>();
+            SimpleIoc.Default.Register<AeroplaneViewModel>();
+            SimpleIoc.Default.Register<TicketViewModel>();
 
             //ServiceLocator.Current.GetInstance<StudentViewModel>();
         }
-        
-        public PilotViewModel PilotVMInstance
-        {
-            get
-            {
-                return ServiceLocator.Current.GetInstance<PilotViewModel>();
-            }
-        }
+
+        public PilotViewModel PilotVMInstance => ServiceLocator.Current.GetInstance<PilotViewModel>();
+
+        public StewardessViewModel StewardessVMInstance => ServiceLocator.Current.GetInstance<StewardessViewModel>();
+
+        public CrewViewModel CrewVMInstance => ServiceLocator.Current.GetInstance<CrewViewModel>();
+
+        public AeroplaneViewModel AeroplaneVMInstance => ServiceLocator.Current.GetInstance<AeroplaneViewModel>();
+
+        public AeroplaneTypeViewModel AeroplaneTypeVMInstance => ServiceLocator.Current.GetInstance<AeroplaneTypeViewModel>();
+
+        public FlightViewModel FlightVMInstance => ServiceLocator.Current.GetInstance<FlightViewModel>();
+
+        public TicketViewModel TickettVMInstance => ServiceLocator.Current.GetInstance<TicketViewModel>();
+
+        public DepartureViewModel DepartureVMInstance => ServiceLocator.Current.GetInstance<DepartureViewModel>();
     }
 }
